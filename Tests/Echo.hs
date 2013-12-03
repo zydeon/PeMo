5af336531bec0291302f26358de42dfa8d1a89c1
Pedro Matias <zydeon@gmail.com>
2013-12-03 17:34:01 +0100

    begin of session retrieval

--- Tests/Echo.hs
+++ Tests/Echo.hs
@@ -3,13 +3,20 @@ module Echo where
 {-# LANGUAGE OverloadedStrings #-}
 
 import Network.Xmpp
+import Network.Socket
+import Data.Text.Internal
 import Control.Monad
 import Data.Default
 import System.Log.Logger
 
-makeSession :: 
-result <- session
-             "example.com"
-              (Just (\_ -> ( [scramSha1 "username" Nothing "password"])
-                           , Nothing))
-              def
+makeSession :: HostName -> Text -> Text -> IO Session --IO (Either XmppFailure Session)
+makeSession d u p = do
+                     result <- session d
+                                   (Just (\_ -> ( [scramSha1 u Nothing p]), Nothing))
+                                   def
+                     case result of 
+                        Right s -> return s
+                        Left e -> error $ "XmppFailure: " ++ (show e)
+
+
+
