5a79921453641dd83f0000d85b9bd500a90a2918
Mozhan <mozhan.soltani@gmail.com>
2013-12-07 19:03:12 +0100

    Update UI.hs. Update UI.hs. Update UI.hs.

--- Source/PeMo/UI.hs
+++ Source/PeMo/UI.hs
@@ -5,33 +5,42 @@ module UI where
 import Graphics.Vty hiding (Button)
 import Graphics.Vty.Widgets.All
 
-uiInit c = do
+uiInit ch = do
 
-  e2 <- multiLineEditWidget
-  e3 <- editWidget
-
+  e1 <- multiLineEditWidget
+  e2 <- editWidget
+  e3 <- multiLineEditWidget
+  
   fg <- newFocusGroup
- -- _  <- addToFocusGroup fg e2
-  _  <- addToFocusGroup fg e3
+ -- _  <- addToFocusGroup fg e1
+  _  <- addToFocusGroup fg e2
 
-  ui <- (bordered e2) <--> (bordered e3)
-  setBoxChildSizePolicy ui (Percentage 90)
+  ui <- (bordered e1)
+        <--> (plainText "Commands: EXIT= Esc   ... ")
+        <--> (bordered e2)
 
-  c <- centered   =<< (
-                         (return ui)
-                         <--> (plainText "Commands: EXIT= ^E  ... ")
-                     --  <--> (plainText "- Esc to quit\n\n- TAB to switch editors") >>= withBoxSpacing 1
+  setBoxChildSizePolicy ui (Percentage 88)
+  
+  bigBox  <- (bordered ui)
+          <++> (bordered e3)
+          
+  setBoxChildSizePolicy bigBox (Percentage 85)
 
-                      )
+  c2 <- hCentered   =<< (
+                        return bigBox
+                        )
+
+       
 
   coll <- newCollection
-  _ <- addToCollection coll c fg
+ -- _ <- addToCollection coll c fg
+  _ <- addToCollection coll c2 fg
 
   fg `onKeyPressed` \_ k _ ->
       case k of
         KEsc -> shutdownUi >> return True
         _ -> return False
 
-  runUi coll $ defaultContext { focusAttr = fgColor yellow }
+  runUi coll $ defaultContext { focusAttr = fgColor blue }
 
 
