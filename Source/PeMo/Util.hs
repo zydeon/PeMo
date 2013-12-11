70db6603147aefbb18ad159fbe8653bc14342edc
Mozhan <mozhan.soltani@gmail.com>
2013-12-11 16:40:22 +0100

    Set idententions in Util.hs

--- Source/PeMo/Util.hs
+++ Source/PeMo/Util.hs
@@ -8,12 +8,12 @@ import IM
 
 mkSession :: IO (Maybe (Session, Jid))
 mkSession = do
-    try <- tryLogin
-    case try of 
-        (Left  failure) -> loginFailed
-        (Right session) -> do
-                            mjid <- getJid' session
-                            return $ Just (session, fromJust mjid)
+          try <- tryLogin
+          case try of 
+              (Left  failure) -> loginFailed
+              (Right session) -> do
+                              mjid <- getJid' session
+                              return $ Just (session, fromJust mjid)
 
 
 loginFailed :: IO (Maybe (Session, Jid))
@@ -23,23 +23,24 @@ loginFailed = do
             if reply == "y" || reply == "Y"
                 then mkSession
                 else if reply == "n" || reply == "N"
-                        then return Nothing
-                        else loginFailed
+                         then return Nothing
+                         else loginFailed
 
 -- Collects the user credentials from the command line.
 getCreds :: IO (String, String, String)
 getCreds = do
-  putStrLn "Username:"
-  usr <- getLine
-  putStrLn "Password:"
-  tc <- getTerminalAttributes stdInput
-  setTerminalAttributes stdInput (withoutMode tc EnableEcho) Immediately
-  pass <- getLine
-  setTerminalAttributes stdInput tc Immediately
-  putStrLn "Domain:"
-  domain <- getLine
-  return (usr, pass, domain)
+         putStrLn "Username:"
+         usr <- getLine
+         putStrLn "Password:"
+         tc <- getTerminalAttributes stdInput
+         setTerminalAttributes stdInput (withoutMode tc EnableEcho) Immediately
+         pass <- getLine
+         setTerminalAttributes stdInput tc Immediately
+         putStrLn "Domain:"
+         domain <- getLine
+         return (usr, pass, domain)
 
 tryLogin :: IO (Either LoginFailure Session)
-tryLogin = do (usr,pass,dom) <- getCreds
-              login dom (pack usr) (pack pass)
+tryLogin = do
+         (usr,pass,dom) <- getCreds
+         login dom (pack usr) (pack pass)
